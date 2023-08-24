a924c82f02937b4ad9d35b1576e140687f16139a
ando <20373221@buaa.edu.cn>
2023-08-24 22:21:16 +0800

    feat: initial file io.

---
+++ src/driver/config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ando on 2023-08-22.
+//
+
+#include "config.h"
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 // Created by Ando on 2023-08-22.
 //
 #include <iostream>
+#include <fstream>
+#include "driver/config.h"
 
-int main(){
-    std::cout << "Hello World!" << '\n';
+int main(int argc, char **argv){
+
+    std::ifstream source("./testcase.sy");
+    std::ofstream output_ir("./llvm.ll");
+
+
+    char c;
+    while (source.get(c)){
+        output_ir << c;
+    }
+
     return 0;
 }
