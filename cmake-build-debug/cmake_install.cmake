8ab001408a12d5e90406cc6c926a2363fa38062f
ando <20373221@buaa.edu.cn>
2023-08-25 16:23:45 +0800

    fix: 修改了CMakeList.txt以使得可以使用MinGW-Clang工具链

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/Program Files (x86)/CLion 2023.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/Program Files (x86)/MSY2/clang64/bin/llvm-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- src/driver/config.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Ando on 2023-08-22.
-//
-
-#include "config.h"
